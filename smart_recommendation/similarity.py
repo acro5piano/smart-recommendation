377c687076cd33b138c83af2febb4412247520e3
gosho-kazuya <ketsume0211@gmail.com>
2016-05-07 14:07:56 +0900

    Add find_recommendations

--- smart_recommendation/similarity.py
+++ smart_recommendation/similarity.py
@@ -1,13 +1,20 @@
 
 import pandas as pd
+import os
+
 from scipy.spatial.distance import cosine
+from IPython import embed
 
 
 DATA_PATH = './data/stretch_data.txt'
+PROGRESS_PATH = './data/progress_table.csv'
+SIMILARITY_PATH = './data/similarity_table.csv'
 
 
 def generate_progress_table():
+    """TODO: map status"""
     df = pd.read_csv(DATA_PATH)
+    #df['LASTSTATUSID'] = df['LASTSTATUSID'].replace({})
     progress_table = df.pivot_table(index='ORDERID', columns='CRID', values='LASTSTATUSID').fillna(0)
     return progress_table
 
@@ -19,20 +26,27 @@ def generate_similarity_table():
     return df
 
 
-def generate_cosine_similarity_table():
-    """Duplicated method"""
-    df = pd.DataFrame(columns=careers, index=careers)
-    for c in careers:
-        for cc in careers:
-            df.ix[c, cc] = get_similarity(progress_table, c, cc)
-    return df
+def find_recommendations(progress_table, similarity_table, career_id):
+    """Return recommend_vector for a career"""
 
+    # Filter orders already applied
+    career_vec = progress_table[career_id]
+    filter_array = career_vec.where(career_vec == 0, -1)
+    filter_array = filter_array.where(filter_array < 0, 1)
+    filter_array = filter_array.where(filter_array > 0, 0)
 
-def get_similarity(progress_table, sub1, sub2):
-    return 1 - cosine(progress_table[sub1], progress_table[sub2])
+    return progress_table.dot(similarity_table[career_id].values) * filter_array
 
 
 if __name__ == '__main__':
-    generate_progress_table().to_csv('tmp/progress_table.csv')
-    generate_similarity_table().to_csv('tmp/similarity_table.csv')
+    if not os.path.exists(PROGRESS_PATH):
+        generate_progress_table().to_csv(PROGRESS_PATH)
+    if not os.path.exists(SIMILARITY_PATH):
+        generate_similarity_table().to_csv(SIMILARITY_PATH)
+
+    progress_table = pd.read_csv(PROGRESS_PATH, index_col='ORDERID')
+    similarity_table = pd.read_csv(SIMILARITY_PATH, index_col='CRID')
+
+    # Get recommended order for career '14'
+    find_recommendations(progress_table, similarity_table, '14').to_csv('tmp/recommend_vector_14.csv')
 
